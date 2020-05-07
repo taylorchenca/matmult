d2b2e0f9e4d87bcd47dce8efe0e3952b6492602c
Haoyao Chen <taylorchenca@gmail.com>
2020-05-07 14:04:12 -0700

    Reduced matrix size.

--- matmult_omp_threadnum.sh
+++ matmult_omp_threadnum.sh
@@ -18,5 +18,5 @@ log_file_name=matmult_omp_threadnum-$current_time.log
 tile_size=32
 for thread_num in 1 2 4 8 12 14 16 20 24 28
 do
-/WAVE/projects/COEN-319-Sp20/hchen7/hw2/matmult/matmult_omp 5000 5000 5000 $tile_size $thread_num >> /WAVE/projects/COEN-319-Sp20/hchen7/hw2/logs/$log_file_name
+/WAVE/projects/COEN-319-Sp20/hchen7/hw2/matmult/matmult_omp 1000 1000 1000 $tile_size $thread_num >> /WAVE/projects/COEN-319-Sp20/hchen7/hw2/logs/$log_file_name
 done
