56e7e27425ada5dcb2bc3b7fb8c5957894c50460
Haoyao Chen <taylorchenca@gmail.com>
2020-05-21 16:05:20 -0700

    Extended time.

--- matmult_omp_ref.sh
+++ matmult_omp_ref.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem-per-cpu=8G
 #SBATCH --nodes=1
 #SBATCH --output=matmult_omp_ref-%j.out
-#SBATCH --time=59:00
+#SBATCH --time=23:59:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hchen7@scu.edu #
 
